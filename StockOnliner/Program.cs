c868839f109e4466072f331457ae98ff39e0b5bf
Илья Ерыкалин <erykalin1986@yandex.ru>
2022-08-09 16:58:41 +0300

    Добавьте файлы проекта.

---
+++ StockOnliner/Program.cs
@@ -0,0 +1,11 @@
+﻿using StockOnliner;
+Console.WriteLine("Bank of America stock price:");
+
+StockExchageMonitor stockExchageMonitor = new StockExchageMonitor();
+stockExchageMonitor.PriceChangeHandler = ShowPrice;
+stockExchageMonitor.Start();
+
+static void ShowPrice(int price)
+{
+    Console.WriteLine($"New price is: {price}");
+}
